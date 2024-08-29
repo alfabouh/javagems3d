b42f8c6adba922fb634601a007293833426cd754
gltexture <gltexture@github.com>
2024-08-29 22:18:15 +0300

    Delete key directory

--- key/ossl/openssl-3/x64/bin/genpfx.bat
+++
@@ -1,4 +0,0 @@
-openssl genpkey -algorithm RSA -out gen/private.key -aes256
-openssl req -new -key gen/private.key -out gen/request.csr
-openssl x509 -req -days 365 -in gen/request.csr -signkey gen/private.key -out gen/certificate.crt
-openssl pkcs12 -export -out gen/certificate.pfx -inkey gen/private.key -in gen/certificate.crt
