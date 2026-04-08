0dde79f74ef93276821f30fbd26ca30f707ff396
gltexture <gltexture@github.com>
2026-04-08 15:40:33 +0300

    08.04.26 wbdev 27_1

--- output/jgems3d-core-run-debug.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-                                    java -Dpolyglotimpl.DisableMultiReleaseCheck=true --enable-native-access=ALL-UNNAMED -XX:+UseG1GC -Xms512m -Xmx4G -cp "./core/jgems3d-core.jar;./core/jgems3d-launcher.jar" launcher.bootstrap.Bootstrap workbench=false debug=true
-                                    pause
